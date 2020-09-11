17224caec7f5afd319473245323ee5a17e76cdab
xiaoyidevil <ltt.007@163.com>
2020-09-11 10:42:44 +0900

    Add files via upload

---
+++ DeductionDemo/DeductionDemo/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace DeductionDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtLocation = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtQuadrant = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtEnd = new System.Windows.Forms.TextBox();
+            this.txtBegin = new System.Windows.Forms.TextBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.txtLocation);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.txtQuadrant);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.txtEnd);
+            this.panel1.Controls.Add(this.txtBegin);
+            this.panel1.Controls.Add(this.btnClear);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel1.Location = new System.Drawing.Point(901, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 682);
+            this.panel1.TabIndex = 3;
+            // 
+            // txtLocation
+            // 
+            this.txtLocation.Location = new System.Drawing.Point(88, 64);
+            this.txtLocation.Name = "txtLocation";
+            this.txtLocation.Size = new System.Drawing.Size(100, 21);
+            this.txtLocation.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(29, 67);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 12);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "location";
+            // 
+            // txtQuadrant
+            // 
+            this.txtQuadrant.Location = new System.Drawing.Point(88, 118);
+            this.txtQuadrant.Name = "txtQuadrant";
+            this.txtQuadrant.Size = new System.Drawing.Size(100, 21);
+            this.txtQuadrant.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 121);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Quadrant";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 12);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "end point";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 12);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "begin point";
+            // 
+            // txtEnd
+            // 
+            this.txtEnd.Location = new System.Drawing.Point(88, 37);
+            this.txtEnd.Name = "txtEnd";
+            this.txtEnd.Size = new System.Drawing.Size(100, 21);
+            this.txtEnd.TabIndex = 5;
+            // 
+            // txtBegin
+            // 
+            this.txtBegin.Location = new System.Drawing.Point(88, 12);
+            this.txtBegin.Name = "txtBegin";
+            this.txtBegin.Size = new System.Drawing.Size(100, 21);
+            this.txtBegin.TabIndex = 4;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(113, 89);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 3;
+            this.btnClear.Text = "清屏";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.trackBar1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 645);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(901, 37);
+            this.panel2.TabIndex = 4;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.trackBar1.Location = new System.Drawing.Point(0, 0);
+            this.trackBar1.Maximum = 1000;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(899, 35);
+            this.trackBar1.TabIndex = 2;
+            this.trackBar1.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(112, 145);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "gif";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1101, 682);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "推演demo";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtEnd;
+        private System.Windows.Forms.TextBox txtBegin;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtQuadrant;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtLocation;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ DeductionDemo/DeductionDemo/Form1.cs
@@ -0,0 +1,282 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DeductionDemo
+{
+    public partial class Form1 : Form
+    {
+        public class DeductionPointDTO {
+            public Point point;
+            public double LenStart = 0;
+            public double LenEnd = 0;
+        }
+        public enum Quadrant
+        {
+            None = 0,
+            First = 1,
+            Second = 2,
+            Third = 3,
+            Forth =4
+        }
+
+        Graphics g = null;
+        Point beginP;
+        Point endP;
+        Point currentP;
+        List<DeductionPointDTO> listP = new List<DeductionPointDTO>();
+        double TotalLen = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            SetStyle(ControlStyles.UserPaint
+            | ControlStyles.ResizeRedraw
+            | ControlStyles.AllPaintingInWmPaint
+            | ControlStyles.OptimizedDoubleBuffer
+            | ControlStyles.UserMouse,
+            true);
+            this.UpdateStyles();
+            g = this.CreateGraphics();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+
+            #region 测试两个点坐标系变换
+            //if (beginP == new Point())
+            //{
+            //    beginP = e.Location;
+            //    txtBegin.Text = "X:" + beginP.X + ",Y:" + beginP.Y;
+            //    Rectangle rectBegin = new Rectangle(beginP, new Size(8, 8));
+            //    rectBegin.Offset(-4, -4);
+            //    g.FillEllipse(new SolidBrush(Color.Yellow), rectBegin);
+            //}
+            //else if (endP == new Point())
+            //{
+            //    endP = e.Location;
+            //    txtEnd.Text = "X:" + endP.X + ",Y:" + endP.Y;
+            //    Rectangle rectEnd = new Rectangle(endP, new Size(8, 8));
+            //    rectEnd.Offset(-4, -4);
+            //    g.FillEllipse(new SolidBrush(Color.Green), rectEnd);
+            //    g.DrawLine(new Pen(Color.Red), beginP, endP);
+            //    txtQuadrant.Text = GetQuadrant(beginP, endP).ToString();
+            //}
+            #endregion
+
+            if (listP.Count == 0)
+            {
+                DeductionPointDTO pt = new DeductionPointDTO();
+                pt.point = e.Location;
+                pt.LenStart = 0;
+                pt.LenEnd = 0;
+                listP.Add(pt);
+            }
+            else
+            {
+                DeductionPointDTO pt = new DeductionPointDTO();
+                pt.point = e.Location;
+                pt.LenStart = listP[listP.Count - 1].LenEnd;
+                double lenX = Math.Abs(listP[listP.Count - 1].point.X - e.Location.X);
+                double lenY = Math.Abs(listP[listP.Count - 1].point.Y - e.Location.Y);
+                double distance = Math.Sqrt(Math.Pow(lenX, 2) + Math.Pow(lenY, 2));
+                TotalLen = TotalLen + distance;
+                pt.LenEnd = TotalLen;
+                listP.Add(pt);
+            }
+
+            for (int i = 0; i < listP.Count; i++)
+            {
+                Rectangle rect = new Rectangle(listP[i].point, new Size(8, 8));
+                rect.Offset(-4, -4);
+                g.FillEllipse(new SolidBrush(Color.Green), rect);
+
+                rect = new Rectangle(listP[i - 1 == -1 ? 0 : i - 1].point, new Size(8, 8));
+                rect.Offset(-4, -4);
+                g.FillEllipse(new SolidBrush(Color.Green), rect);
+
+                g.DrawLine(new Pen(Color.Red), listP[i - 1 == -1 ? 0 : i - 1].point, listP[i].point);
+            }
+        }
+
+        private Quadrant GetQuadrant(Point beginPoint, Point endPoint) {
+            Quadrant quadrant = Quadrant.None;
+
+            if (beginPoint.X < endPoint.X && beginPoint.Y >= endPoint.Y) {
+                quadrant = Quadrant.First;
+            }
+            if (beginPoint.X <= endPoint.X && beginPoint.Y < endPoint.Y) {
+                quadrant = Quadrant.Second;
+            }
+            if (beginPoint.X > endPoint.X && beginPoint.Y <= endPoint.Y) {
+                quadrant = Quadrant.Third;
+            }
+            if (beginPoint.X >= endPoint.X && beginPoint.Y > endPoint.Y) {
+                quadrant = Quadrant.Forth;
+            }
+            return quadrant;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            g.Clear(this.BackColor);
+            listP.Clear();
+            beginP = new Point();
+            endP = new Point();
+            txtBegin.Text = "";
+            txtEnd.Text = "";
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            txtLocation.Text = "X:" + e.Location.X + ",Y:" + e.Location.Y;
+        }
+
+        private Point GetCurrentPoint(Point beginPoint, Point endPoint,int value)
+        {
+            Point point = new Point();
+
+            Quadrant quadrant = GetQuadrant(beginPoint, endPoint);
+
+            double lenX = Math.Abs(beginPoint.X - endPoint.X);
+            double lenY = Math.Abs(beginPoint.Y - endPoint.Y);
+            double distance = Math.Sqrt(Math.Pow(lenX, 2) + Math.Pow(lenY, 2));
+            double currentDistance = distance / 100 * value;
+            double angle = 0;
+            int x = beginPoint.X;
+            int y = beginPoint.Y;
+
+            switch (quadrant)
+            {
+                case Quadrant.None:
+                    break;
+                case Quadrant.First:
+                    angle = Math.Atan(lenY / lenX);;
+                    x = beginPoint.X + (int)(Math.Cos(angle) * currentDistance);
+                    y = beginPoint.Y - (int)(Math.Sin(angle) * currentDistance);
+                    point = new Point(x, y);
+                    break;
+                case Quadrant.Second:
+                    angle = Math.Atan(lenY / lenX); ;
+                    x = beginPoint.X + (int)(Math.Cos(angle) * currentDistance);
+                    y = beginPoint.Y + (int)(Math.Sin(angle) * currentDistance);
+                    point = new Point(x, y);
+                    break;
+                case Quadrant.Third:
+                    angle = Math.Atan(lenY / lenX); ;
+                    x = beginPoint.X - (int)(Math.Cos(angle) * currentDistance);
+                    y = beginPoint.Y + (int)(Math.Sin(angle) * currentDistance);
+                    point = new Point(x, y);
+                    break;
+                case Quadrant.Forth:
+                    angle = Math.Atan(lenY / lenX); ;
+                    x = beginPoint.X - (int)(Math.Cos(angle) * currentDistance);
+                    y = beginPoint.Y - (int)(Math.Sin(angle) * currentDistance);
+                    point = new Point(x, y);
+                    break;
+                default:
+                    break;
+            }
+
+            return point;
+        }
+
+        private void trackBar1_ValueChanged(object sender, EventArgs e)
+        {
+
+            //g.Clear(this.BackColor);
+
+            #region 测试两个点坐标系变换
+            //Rectangle rectBegin = new Rectangle(beginP, new Size(8, 8));
+            //rectBegin.Offset(-4, -4);
+            //g.FillEllipse(new SolidBrush(Color.Green), rectBegin);
+
+            //Rectangle rectEnd = new Rectangle(endP, new Size(8, 8));
+            //rectEnd.Offset(-4, -4);
+            //g.FillEllipse(new SolidBrush(Color.Green), rectEnd);
+            //g.DrawLine(new Pen(Color.Red), beginP, endP);
+
+
+            //currentP = GetCurrentPoint(beginP, endP, trackBar1.Value);
+            //Rectangle rect = new Rectangle(currentP, new Size(8, 8));
+            //rect.Offset(-4, -4);
+            //g.FillEllipse(new SolidBrush(Color.Green), rect);
+            #endregion
+
+            for (int i = 0; i < listP.Count; i++)
+            {
+                Rectangle rect = new Rectangle(listP[i].point, new Size(8, 8));
+                rect.Offset(-4, -4);
+                g.FillEllipse(new SolidBrush(Color.Green), rect);
+
+                rect = new Rectangle(listP[i - 1 == -1 ? 0 : i - 1].point, new Size(8, 8));
+                rect.Offset(-4, -4);
+                g.FillEllipse(new SolidBrush(Color.Green), rect);
+
+                g.DrawLine(new Pen(Color.Red), listP[i - 1 == -1 ? 0 : i - 1].point, listP[i].point);
+            }
+
+            currentP = GetPointDTO(trackBar1.Value, trackBar1.Maximum);
+            Rectangle rectCurrent = new Rectangle(currentP, new Size(8, 8));
+            rectCurrent.Offset(-4, -4);
+            g.FillEllipse(new SolidBrush(Color.Green), rectCurrent);
+        }
+
+        private Point GetPointDTO(int value,int MaxValue) {
+
+            Point point = new Point();
+            DeductionPointDTO beginPT = new DeductionPointDTO();
+            DeductionPointDTO endPT = new DeductionPointDTO();
+            int beginInt = 0;
+            int CurrentPosition = 0;
+
+            double currentDistance = TotalLen / MaxValue * value;
+            endPT = listP.Find((DeductionPointDTO tempPt) => tempPt.LenStart <= currentDistance && tempPt.LenEnd >= currentDistance);
+            if (endPT == null) {
+                endPT = listP[listP.Count - 1];
+            }
+            beginInt = listP.FindIndex((DeductionPointDTO tempPt) => tempPt.LenStart <= currentDistance && tempPt.LenEnd >= currentDistance) - 1;
+            
+            if (beginInt <= 0)
+            {
+                beginPT = listP[0];
+            }
+            else {
+                beginPT = listP[beginInt];
+            }
+            
+            CurrentPosition = (int)((currentDistance - endPT.LenStart) / (endPT.LenEnd - endPT.LenStart) * 100);
+
+            point = GetCurrentPoint(beginPT.point, endPT.point, CurrentPosition);
+
+            return point;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Bitmap b = new Bitmap(@"C:\Users\Administrator\Desktop\aa.gif");
+            Guid[] guid = b.FrameDimensionsList;
+            FrameDimension fd = new FrameDimension(guid[0]);
+
+            int count = b.GetFrameCount(fd);
+
+            for (int i = 0; i < count-1; i++)
+            {
+                b.SelectActiveFrame(fd, i);
+                g.DrawImage(b, new Point(0, 0));
+            }
+        }
+    }
+}
